35149bb35275ee215f03375727247909c15a9529
Ole Schütt <ole.schuett@cp2k.org>
2019-03-10 14:54:49 +0100

    Add startup message to cleanup.sh

--- toolbox/cleanup.sh
+++ toolbox/cleanup.sh
@@ -4,6 +4,8 @@
 
 # Remove GCP container images that are not tagged. Typically. the only tag is "latest".
 
+echo "Looking for old images..."
+
 for IMAGE in $(gcloud container images list --format="get(name)"); do
    for SHA in $(gcloud container images list-tags --filter='-tags:(latest)' --format='get(digest)' $IMAGE); do
        gcloud --quiet container images delete --force-delete-tags ${IMAGE}@${SHA}
