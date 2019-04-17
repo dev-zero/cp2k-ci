81f3ddf523050d75d5773ae12ee0ff3d71cf7729
Ole Schütt <ole.schuett@cp2k.org>
2019-04-17 23:29:16 +0200

    Add build_target_new.sh

--- toolbox/build_target_new.sh
+++ toolbox/build_target_new.sh
@@ -5,17 +5,18 @@
 set -eo pipefail
 
 if (( $# < 4 )) ; then
-    echo "Usage: build_target.sh <TARGET> <REPO> <DOCKERFILE> <REPORT_UPLOAD_URL> [build-args]"
+    echo "Usage: build_target.sh <REPO> <GIT_REF> <DOCKERFILE> <IMAGE_TAG> <REPORT_UPLOAD_URL> [build-args]"
     exit 1
 fi
 
-TARGET=$1
-REPO=$2
+REPO=$1
+GIT_REF=$2
 DOCKERFILE=$3
-REPORT_UPLOAD_URL=$4
-shift 4
+IMAGE_TAG=$4
+REPORT_UPLOAD_URL=$5
+shift 5
 
-REPORT=/tmp/build_${TARGET}_report.txt
+REPORT=/tmp/build_report.txt
 
 echo -n "StartDate: "                     |& tee -a $REPORT
 date --utc --rfc-3339=seconds             |& tee -a $REPORT
@@ -24,24 +25,23 @@ echo -n "CpuId:"                          |& tee -a $REPORT
 cpuid -1 | grep "(synth)"                 |& tee -a $REPORT
 
 /opt/cp2kci-toolbox/start_docker.sh       |& tee -a $REPORT
-
-PROJECT=$(gcloud config list --format 'value(core.project)')
-IMAGE_TAG="gcr.io/${PROJECT}/img_${TARGET}:latest"
 REPO_URL="https://github.com/cp2k/${REPO}.git"
 BUILD_DATE=$(date --rfc-3339=seconds --utc)
 
 cd /tmp
 git clone --depth=1 --single-branch -b master ${REPO_URL} |& tee -a $REPORT
-cd ${REPO}$(dirname ${DOCKERFILE})
-GIT_SHA=$(git rev-parse HEAD)
+cd ${REPO}
+git fetch origin ${GIT_REF}                               |& tee -a $REPORT
+git -c advice.detachedHead=false checkout ${GIT_REF}      |& tee -a $REPORT
+git submodule update --init --recursive                   |& tee -a $REPORT
+cd (dirname ${DOCKERFILE})
 git --no-pager log -1 --pretty='%nCommitSHA: %H%nCommitTime: %ci%nCommitAuthor: %an%nCommitSubject: %s%n' . |& tee -a $REPORT
 
 docker build \
  --label "org.label-schema.schema-version=1.0" \
- --label "org.label-schema.name=${TARGET}" \
  --label "org.label-schema.vendor=CP2K-CI" \
  --label "org.label-schema.vcs-url=${REPO_URL}" \
- --label "org.label-schema.vcs-ref=${GIT_SHA}" \
+ --label "org.label-schema.vcs-ref=${GIT_REF}" \
  --label "org.label-schema.build-date=${BUILD_DATE}" \
  --tag ${IMAGE_TAG} --file $(basename ${DOCKERFILE}) "$@" . |& tee -a $REPORT
 
