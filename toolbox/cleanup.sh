3df879e41358671680c696d0c9ecfa8caa5d7fb5
Ole Schütt <ole.schuett@cp2k.org>
2019-05-13 11:17:13 +0200

    Remove GCP images earlier

--- toolbox/cleanup.sh
+++ toolbox/cleanup.sh
@@ -2,15 +2,24 @@
 
 # author: Ole Schuett
 
-# Remove GCP container images that are not tagged. Typically. the only tag is "latest".
+# Remove old GCP container images.
 
 echo "Looking for old images..."
-MAX_AGE_DAYS=30
 
 for IMAGE in $(gcloud container images list --format="get(name)"); do
-   for SHA in $(gcloud container images list-tags --filter="timestamp.datetime < -P${MAX_AGE_DAYS}D" --format="get(digest)" $IMAGE); do
-       gcloud --quiet container images delete --force-delete-tags ${IMAGE}@${SHA}
-   done
+    if [[ $IMAGE == *"img_cp2kci_"* ]]; then
+        FILTER="-tags:(latest)"
+    elif [[ $IMAGE == *"img_cp2k-toolchain"* ]]; then
+        MAX_AGE_DAYS=30
+        FILTER="timestamp.datetime < -P${MAX_AGE_DAYS}D AND -tags:(master)"
+    else
+        MAX_AGE_DAYS=3
+        FILTER="timestamp.datetime < -P${MAX_AGE_DAYS}D"
+    fi
+
+    for SHA in $(gcloud container images list-tags --filter="${FILTER}" --format="get(digest)" "${IMAGE}"); do
+        gcloud --quiet container images delete --force-delete-tags "${IMAGE}@${SHA}"
+    done
 done
 
 # Cloud storage is cleaned via lifecycle management.
